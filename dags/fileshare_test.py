593b9cb62cef1c28c65a91cf8337a50a5b34a2d3
msand30 <151659866+msand30@users.noreply.github.com>
2024-08-25 13:23:57 -0700

    Add files via upload

---
+++ dags/fileshare_test.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.samba.hooks.samba import SambaHook
+
+with DAG(
+    dag_id='lfs_poc_download',
+    start_date=dt.datetime(2024,8,22,9),
+    schedule="@daily",
+    catchup=False,
+    max_active_runs=1,
+    default_args={
+        "retries": 0,
+        "retry_delay" : dt.timedelta(minutes=2)
+    }
+) as dag: 
+    #download last month's LFS file via the task API
+    @task()
+    def fileshare_test():
+        hook = SambaHook(smb_conn_id)
+        files = hook.listdir('Airflow_POC_Dev/')
+        print("Files in the given directory:")
+        for f in files:
+            print(f)
+
+fileshare_test()
