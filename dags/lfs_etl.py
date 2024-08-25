61af90eb15daa961319c838a15bf6fc525b1134b
msand30 <151659866+msand30@users.noreply.github.com>
2024-08-25 12:20:56 -0700

    Add files via upload

--- dags/lfs_etl.py
+++ dags/lfs_etl.py
@@ -37,9 +37,9 @@ def lfs_load ():
         filebytes = BytesIO()
 
         zf.download_fileobj(filebytes)
-        file = zipfile.ZipFile(filebytes)
+        zip_file = zipfile.ZipFile(filebytes)
 
-        df = pd.read_csv(zf.open('pub0724.csv'))
+        df = pd.read_csv(zip_file.open('pub0724.csv'))
         return df
         
     '''
