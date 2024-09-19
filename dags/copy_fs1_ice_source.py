712c9533c11c66fdf329cb644f9ea71743f77054
eloynav <81321529+eloynav@users.noreply.github.com>
2024-09-19 11:20:49 -0700

    Add files via upload

--- dags/copy_fs1_ice_source.py
+++ dags/copy_fs1_ice_source.py
@@ -32,8 +32,7 @@ def test_ice_connection():
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    #'start_date': datetime(2024, 9, 19),
-    'start_date' : pendulum.datetime(2024, 09, 19, hour=8, tz="PDT")
+    'start_date': datetime(2024, 9, 19),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
