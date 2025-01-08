6c0395b3ce02a8a7e4805eb50602a4675d614ea3
eloynav <81321529+eloynav@users.noreply.github.com>
2025-01-08 14:45:03 -0800

    Add files via upload

--- dags/fs1_ice_ps.py
+++ dags/fs1_ice_ps.py
@@ -5,28 +5,9 @@ from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.contrib.hooks.ssh_hook import SSHHook
 from datetime import datetime
 
-conn_id = 'ssh_powershell'
-sshHook = SSHHook.get_conn(ssh_conn_id = conn_id)
+#conn_id = 'ssh_powershell'
+sshHook = SSHHook.get_conn(ssh_conn_id = "ssh_powershell")
 
-
-def remote_powershell():
-    # Replace these with your SMB server details
-#    conn_id = 'fs1_fin_data_store'
-      
-    directory = '/rmo_ct_prod/'
-    path = directory
-#    ssh = SSHHook(ssh_conn_id=conn_id)
-    
-    ssh_client = None
-    
-    try:
-        ssh_client = ssh.get_conn()
-        ssh_cclient.load_system_host_keys()
-        ssh_client.exec_command('ps_remote.ps1')
-    finally:
-        if ssh_client:
-            ssh_client.close()
-        
 
 default_args = {
     'owner': 'airflow',
@@ -46,7 +27,7 @@ dag = DAG(
 
 ssh_task = SSHOperator(
     task_id = 'ssh_task',
-    ssh_conn_id = 'ssh_powershell',
+    ssh_conn_id = sshHook,
     command = 'dir /rmo_ct_prod/*.* > files.txt"',
     dag=dag,
 )
