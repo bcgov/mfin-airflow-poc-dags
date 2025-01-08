45620f288ae30317ced0d7065026ed939df2c34c
eloynav <81321529+eloynav@users.noreply.github.com>
2025-01-08 14:35:52 -0800

    Add files via upload

---
+++ dags/fs1_ice_ps.py
@@ -0,0 +1,54 @@
+import os
+import datetime as dt
+from airflow import DAG
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.contrib.hooks.ssh_hook import SSHHook
+from datetime import datetime
+
+conn_id = 'ssh_powershell'
+sshHook = SSHHook.get_conn(ssh_conn_id = conn_id)
+
+
+def remote_powershell():
+    # Replace these with your SMB server details
+#    conn_id = 'fs1_fin_data_store'
+      
+    directory = '/rmo_ct_prod/'
+    path = directory
+#    ssh = SSHHook(ssh_conn_id=conn_id)
+    
+    ssh_client = None
+    
+    try:
+        ssh_client = ssh.get_conn()
+        ssh_cclient.load_system_host_keys()
+        ssh_client.exec_command('ps_remote.ps1')
+    finally:
+        if ssh_client:
+            ssh_client.close()
+        
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 9, 19),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+dag = DAG(
+    'Remote_PowerShell_script',
+    default_args=default_args,
+    description='Executing remote PowerShell script',
+    schedule=None,
+)
+
+ssh_task = SSHOperator(
+    task_id = 'ssh_task',
+    ssh_conn_id = 'ssh_powershell',
+    command = 'dir /rmo_ct_prod/*.* > files.txt"',
+    dag=dag,
+)
+
+ssh_task
