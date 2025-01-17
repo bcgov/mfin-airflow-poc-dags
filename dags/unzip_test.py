70d07f8296557e9b0e3e7db870fe99aff2e1dd39
eloynav <81321529+eloynav@users.noreply.github.com>
2025-01-17 13:34:16 -0800

    Add files via upload

--- dags/unzip_test.py
+++ dags/unzip_test.py
@@ -2,12 +2,14 @@ import os
 import datetime as dt
 import subprocess
 import logging
-from zipfile import ZipFile
+import airflow.operators
+#from zipfile import ZipFile
 from airflow import DAG
 from airflow.providers.samba.hooks.samba import SambaHook
 from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.hooks.fs_hook import FSHook
 from datetime import datetime
+from airflow.operators import UnzipOperator
 
 
 def unzip_test():
@@ -21,31 +23,23 @@ def unzip_test():
     path_zip = directory_zip_file
     #path_unzip = directory_unzip_file
 
-    #hook = SambaHook(conn_id)
+    hook = SambaHook(conn_id)
     
-    hook2 = FSHook(conn_id)
-    base_path = hook2.get_path()
-    with open(os.path.join(base_path, 'iceDB_ICE_BCMOFRMO.zip'), 'r') as fp:
-        print(fp.read())
-    
-    #files = hook.listdir(path_zip)
+    files = hook.listdir(path_zip)
 
 
     #dYmd = dt.datetime.today().strftime('%Y%m%d')
 
-    #for f in files:
-    #    dir_unzip = hook.path
+    for f in files:
  
-     #   print('Current directory: ',dir_unzip)
-        
-#        if f == 'iceDB_ICE_BCMOFRMO.zip' :
+        if f == 'iceDB_ICE_BCMOFRMO.zip' :
 #            logging.info("Extracting all the content '"+ f +"' to '"+ str(path_unzip) +"'")
 #            print('opening zip file')          
-#            with ZipFile(f,'r') as zip_file:
-#                print('Printing all contents of the zip file')
+            with ZipFile("'\\\\fs1.fin.gov.bc.ca\rmo_ct_prod\'",'r') as zip_file:
+                print('Printing all contents of the zip file')
 #                zip_file.print()
 #                zip_file.extractall()
-                #zip_file.extractall(path_unzip)
+                zip_file.extractall("'\\\\fs1.fin.gov.bc.ca\rmo_ct_prod\'")
              #   myzip.readlines
                 #zip_file.close()
             #hook.replace(path + f, destination + 'iceDB_ICE_BCMOFRMO-' + dYmd+'.zip')
