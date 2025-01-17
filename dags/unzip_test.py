a2dee9534d99f3a137321ec666b59bc8b2eff543
eloynav <81321529+eloynav@users.noreply.github.com>
2025-01-17 13:48:37 -0800

    Add files via upload

--- dags/unzip_test.py
+++ dags/unzip_test.py
@@ -2,6 +2,7 @@ import os
 import datetime as dt
 import subprocess
 import logging
+import mmap
 import airflow.operators
 from zipfile import ZipFile
 from airflow import DAG
@@ -34,11 +35,17 @@ def unzip_test():
         if f == 'iceDB_ICE_BCMOFRMO.zip' :
 #            logging.info("Extracting all the content '"+ f +"' to '"+ str(path_unzip) +"'")
 #            print('opening zip file')          
-            with ZipFile("'\\fs1.fin.gov.bc.ca\rmo_ct_prod\'",'r') as zip_file:
+            with open(f,'rb') as zf:
+                m = mmap.mmap(zf.fileno(),0, prot=mmap.PROT_READ)
+                
+                data = m.readline()
+                while data:
+                    print('1')
+                    data = m.readline()
                 print('Printing all contents of the zip file')
 #                zip_file.print()
 #                zip_file.extractall()
-                zip_file.extractall("'\\fs1.fin.gov.bc.ca\rmo_ct_prod\'")
+                #zip_file.extractall("'\\fs1.fin.gov.bc.ca\rmo_ct_prod\'")
              #   myzip.readlines
                 #zip_file.close()
             #hook.replace(path + f, destination + 'iceDB_ICE_BCMOFRMO-' + dYmd+'.zip')
