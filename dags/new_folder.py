9ff7c2a1a6c71718089f1738278e02a30b562bb5
eloynav <81321529+eloynav@users.noreply.github.com>
2025-01-20 10:08:32 -0800

    Add files via upload

---
+++ dags/new_folder.py
@@ -0,0 +1,20 @@
+from airflow.decorators import dag
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+@dag(
+  dag_id="new_rmo_ct_folder",
+  start_date=datetime(2025, 1 ,20),
+  schedule=None,
+  retry=None,
+  )
+  
+def new_rmo_folder_dag():
+    new_folder = BashOperator(
+	    task_kd="new_rmo_folder",
+		bash_command="mkdir $AIRFLOW_HOME/rmo_ct_prod",
+	)
+
+new_folder
+	
+
