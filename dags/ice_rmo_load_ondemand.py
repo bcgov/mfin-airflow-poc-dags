d96684bd513fc3f445a28425415d6ea600f7322e
eloynav <81321529+eloynav@users.noreply.github.com>
2025-01-22 14:40:11 -0800

    Add files via upload

--- dags/ice_rmo_load_ondemand.py
+++ dags/ice_rmo_load_ondemand.py
@@ -45,7 +45,8 @@ def ice_rmo_load_ondemand():
     @task
     def ondemand_load_data():
         
-        source_file_set = ["Stat_QueueActivity_D20241231.csv"]
+        source_file_set = ["Stat_QueueActivity_D20241101.csv","Stat_QueueActivity_D20241102.csv","Stat_QueueActivity_D20241103.csv",
+                           "Stat_QueueActivity_D20241104.csv","Stat_QueueActivity_D20241105.csv"]
         
         for source_file in source_file_set:
             ondemand_load_source(source_file)
