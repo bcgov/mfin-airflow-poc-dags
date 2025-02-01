acdbfbe030b112d41b9830075f95763ad530faaf
eloynav <81321529+eloynav@users.noreply.github.com>
2025-01-31 16:36:46 -0800

    Add files via upload

--- dags/ice_rmo_load_ondemand.py
+++ dags/ice_rmo_load_ondemand.py
@@ -44,11 +44,11 @@ def ice_rmo_load_ondemand():
     @task
     def ondemand_load_data():
         
-        source_file_set = ["icePay_D20240908.csv"]
-        #source_file_set = ["icePay_D20241101.csv", "icePay_D20241102.csv", "icePay_D20241103.csv", "icePay_D20241104.csv",
-        #                   "icePay_D20241105.csv", "icePay_D20241106.csv", "icePay_D20241107.csv", "icePay_D20241108.csv",
-        #                   "icePay_D20241109.csv", "icePay_D20241110.csv", "icePay_D20241111.csv", "icePay_D20241112.csv",
-        #                   "icePay_D20241113.csv", "icePay_D20241114.csv", "icePay_D20241115.csv", "icePay_D20241116.csv"]
+        source_file_set = ["icePay_D20241117.csv", "icePay_D20241118.csv", "icePay_D20241119.csv", "icePay_D20241120.csv",
+                           "icePay_D20241121.csv", "icePay_D20241122.csv", "icePay_D20241123.csv", "icePay_D20241124.csv",
+                           "icePay_D20241125.csv", "icePay_D20241126.csv", "icePay_D20241127.csv", "icePay_D20241128.csv",
+                           "icePay_D20241129.csv", "icePay_D20241130.csv"]#, "icePay_D20241111.csv", "icePay_D20241112.csv",
+                           #"icePay_D20241113.csv", "icePay_D20241114.csv", "icePay_D20241115.csv", "icePay_D20241116.csv"]
 
                            #"icePay_D20240919.csv", "icePay_D20240920.csv"]
         #source_file_set = ["Stat_AgentNotReadyBreakdown_D20250121.csv","Stat_AgentNotReadyBreakdown_D20250122.csv","Stat_AgentNotReadyBreakdown_D20250123.csv",
