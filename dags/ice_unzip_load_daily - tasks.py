a69b477aebee9a4dc56fbd585bf07160aa0c60c7
eloynav <81321529+eloynav@users.noreply.github.com>
2025-02-19 19:09:46 -0800

    Add files via upload

--- dags/ice_unzip_load_daily - tasks.py
+++ dags/ice_unzip_load_daily - tasks.py
@@ -74,63 +74,62 @@ def daily_load_data():
     @task
     # Slowly changin dimension TBD on AgentAssignment, TeamAssignment
     
-        def daily_load_source(psource_file):
-            sql_hook = MsSqlHook(mssql_conn_id='mssql_conn_bulk')
+    def daily_load_source():
+        sql_hook = MsSqlHook(mssql_conn_id='mssql_conn_bulk')
+        conn = sql_hook.get_conn()
+        cursor = conn.cursor()
             
-            source_file_set = ["ACDQueue.csv","Agent.csv","AudioMessage.csv", "AgentAssignment.csv", "AgentSkill.csv",
-                               "ContactLink.csv","ContactSegment.csv",
-                               "Email.csv","EmailGroup.csv","Eval_Contact.csv","EvalScore.csv","EvalCategory.csv","EvalCategoryLangString.csv",
-                               "EvalCriteria.csv","EvalCriteriaLangString.csv","EvalCriteriaValue.csv","EvalCriteriaValueLangString.csv",
-                               "EvalEvaluation.csv","EvalForm.csv","EvalFormLangString.csv",
-                               "Holiday.csv","IMRecording.csv","icePay.csv",
-                               "Languages.csv","LOBCategory.csv","LOBCategoryLangString.csv","LOBCode.csv","LOBCodeLangString.csv",
-                               "Node.csv","NotReadyReason.csv","NotReadyReasonLangString.csv",
-                               "OperatingDates.csv",
-                               "Recordings.csv","RecordingsFaultedFiles.csv","RequiredSkill.csv", 
-                               "SegmentAgent.csv","SegmentQueue.csv","Server.csv","Site.csv","Skill.csv","Stat_CDR_LastSummarized.csv","Switch.csv",
-                               "Stat_AgentActivity_D.csv", "Stat_AgentActivity_I.csv", "Stat_AgentActivity_M.csv", "Stat_AgentActivity_W.csv", "Stat_AgentActivity_Y.csv",
-                               "Stat_AgentActivityByQueue_D.csv", "Stat_AgentActivityByQueue_I.csv", "Stat_AgentActivityByQueue_M.csv", "Stat_AgentActivityByQueue_W.csv", "Stat_AgentActivityByQueue_Y.csv",
-                               "Stat_AgentLineOfBusiness_D.csv", "Stat_AgentLineOfBusiness_I.csv", "Stat_AgentLineOfBusiness_M.csv", "Stat_AgentLineOfBusiness_W.csv", "Stat_AgentLineOfBusiness_Y.csv",
-                      #        "Stat_AgentNotReadyBreakdown_D" 2024 missing Jan30-Feb, 
-                               "Stat_AgentNotReadyBreakdown_M.csv",
-                               "Stat_AgentNotReadyBreakdown_I.csv", "Stat_AgentNotReadyBreakdown_W.csv", "Stat_AgentNotReadyBreakdown_Y.csv",
-                               "Stat_DNISActivity_D.csv", "Stat_DNISActivity_I.csv", "Stat_DNISActivity_M.csv", "Stat_DNISActivity_W.csv", "Stat_DNISActivity_Y.csv",
-                               "Stat_CDR.csv","Stat_CDR_LastSummarize.csv","Stat_CDR_Summary.csv",
-                               "Stat_ADR.csv",
-                               "Stat_QueueActivity_D.csv","Stat_QueueActivity_M.csv","Stat_QueueActivity_I.csv", "Stat_QueueActivity_W.csv", "Stat_QueueActivity_Y.csv",
-                               "Stat_SkillActivity_D.csv", "Stat_SkillActivity_I.csv", "Stat_SkillActivity_M.csv", "Stat_SkillActivity_W.csv", "Stat_SkillActivity_Y.csv",
-                               "Stat_TrunckActivity_D.csv", "Stat_TrunckActivity_I.csv", "Stat_TrunckActivity_M.csv", "Stat_TrunckActivity_W.csv", "Stat_TrunckActivity_Y.csv",    
-                               "Stat_WorkflowActionActivity_D.csv", "Stat_WorkflowActionActivity_I.csv", "Stat_WorkflowActionActivity_M.csv", "Stat_WorkflowActionActivity_W.csv", "Stat_WorkflowActionActivity_Y.csv",
-                               "Team.csv","TeamAssignment.csv",
-                               "UCAddress.csv","UCGroup.csv",
-                               "WfAttributeDetail.csv","WfLinkDetail.csv","WfLink.csv","WfAction.csv","WfPage.csv","WfGraph.csv",
-                               "WfSubAppMethod.csv","WfSubApplication.csv","WfVariables.csv"]
+        source_file_set = ["ACDQueue.csv","Agent.csv","AudioMessage.csv", "AgentAssignment.csv", "AgentSkill.csv",
+                           "ContactLink.csv","ContactSegment.csv",
+                           "Email.csv","EmailGroup.csv","Eval_Contact.csv","EvalScore.csv","EvalCategory.csv","EvalCategoryLangString.csv",
+                           "EvalCriteria.csv","EvalCriteriaLangString.csv","EvalCriteriaValue.csv","EvalCriteriaValueLangString.csv",
+                           "EvalEvaluation.csv","EvalForm.csv","EvalFormLangString.csv",
+                           "Holiday.csv","IMRecording.csv","icePay.csv",
+                           "Languages.csv","LOBCategory.csv","LOBCategoryLangString.csv","LOBCode.csv","LOBCodeLangString.csv",
+                           "Node.csv","NotReadyReason.csv","NotReadyReasonLangString.csv",
+                           "OperatingDates.csv",
+                           "Recordings.csv","RecordingsFaultedFiles.csv","RequiredSkill.csv", 
+                           "SegmentAgent.csv","SegmentQueue.csv","Server.csv","Site.csv","Skill.csv","Stat_CDR_LastSummarized.csv","Switch.csv",
+                           "Stat_AgentActivity_D.csv", "Stat_AgentActivity_I.csv", "Stat_AgentActivity_M.csv", "Stat_AgentActivity_W.csv", "Stat_AgentActivity_Y.csv",
+                           "Stat_AgentActivityByQueue_D.csv", "Stat_AgentActivityByQueue_I.csv", "Stat_AgentActivityByQueue_M.csv", "Stat_AgentActivityByQueue_W.csv", "Stat_AgentActivityByQueue_Y.csv",
+                           "Stat_AgentLineOfBusiness_D.csv", "Stat_AgentLineOfBusiness_I.csv", "Stat_AgentLineOfBusiness_M.csv", "Stat_AgentLineOfBusiness_W.csv", "Stat_AgentLineOfBusiness_Y.csv",
+                      #    "Stat_AgentNotReadyBreakdown_D" 2024 missing Jan30-Feb, 
+                           "Stat_AgentNotReadyBreakdown_M.csv",
+                           "Stat_AgentNotReadyBreakdown_I.csv", "Stat_AgentNotReadyBreakdown_W.csv", "Stat_AgentNotReadyBreakdown_Y.csv",
+                           "Stat_DNISActivity_D.csv", "Stat_DNISActivity_I.csv", "Stat_DNISActivity_M.csv", "Stat_DNISActivity_W.csv", "Stat_DNISActivity_Y.csv",
+                           "Stat_CDR.csv","Stat_CDR_LastSummarize.csv","Stat_CDR_Summary.csv",
+                           "Stat_ADR.csv",
+                           "Stat_QueueActivity_D.csv","Stat_QueueActivity_M.csv","Stat_QueueActivity_I.csv", "Stat_QueueActivity_W.csv", "Stat_QueueActivity_Y.csv",
+                           "Stat_SkillActivity_D.csv", "Stat_SkillActivity_I.csv", "Stat_SkillActivity_M.csv", "Stat_SkillActivity_W.csv", "Stat_SkillActivity_Y.csv",
+                           "Stat_TrunckActivity_D.csv", "Stat_TrunckActivity_I.csv", "Stat_TrunckActivity_M.csv", "Stat_TrunckActivity_W.csv", "Stat_TrunckActivity_Y.csv",    
+                           "Stat_WorkflowActionActivity_D.csv", "Stat_WorkflowActionActivity_I.csv", "Stat_WorkflowActionActivity_M.csv", "Stat_WorkflowActionActivity_W.csv", "Stat_WorkflowActionActivity_Y.csv",
+                           "Team.csv","TeamAssignment.csv",
+                           "UCAddress.csv","UCGroup.csv",
+                           "WfAttributeDetail.csv","WfLinkDetail.csv","WfLink.csv","WfAction.csv","WfPage.csv","WfGraph.csv",
+                           "WfSubAppMethod.csv","WfSubApplication.csv","WfVariables.csv"]
 
-            try:
-                for source_file in source_file_set:
-                
-                    xlen = len(source_file)-4
-                    pTableName = "ICE_" + source_file[:xlen]
-                    conn = sql_hook.get_conn()
-                    cursor = conn.cursor()
+        try:
+            for source_file in source_file_set:
+                xlen = len(source_file)-4
+                pTableName = "ICE_" + source_file[:xlen]
             
-                    query = f""" BULK INSERT [FIN_SHARED_LANDING_DEV].[dbo].{pTableName}
-                            FROM '\\\\fs1.fin.gov.bc.ca\\rmo_ct_prod\\inprogress\\{source_file}'
-                            WITH
-                        ( FORMAT = 'CSV'
-                            );
+                query = f""" BULK INSERT [FIN_SHARED_LANDING_DEV].[dbo].{pTableName}
+                        FROM '\\\\fs1.fin.gov.bc.ca\\rmo_ct_prod\\inprogress\\{source_file}'
+                        WITH
+                      ( FORMAT = 'CSV'
+                        );
                         """
 
-                    start_time = time.time()
-                    cursor.execute(query)
-                    conn.commit()
-                    end_time = time.time()
+                start_time = time.time()
+                cursor.execute(query)
+                conn.commit()
+                end_time = time.time()
             
-                    logging.info(f"bulk insert lapse: --- {psoiurce_file}.csv {time.time() - start_time} seconds ---")          
-                    #print(f"bulk insert duration: --- {time.time() - start_time} seconds ---")
+                logging.info(f"bulk insert lapse: --- {psoiurce_file}.csv {time.time() - start_time} seconds ---")          
+                #print(f"bulk insert duration: --- {time.time() - start_time} seconds ---")
         
-            except Exception as e:
-                print(f"Error loading {psource_file}.csv")
+        except Exception as e:
+            print(f"Error loading {psource_file}.csv")
         
    
     
