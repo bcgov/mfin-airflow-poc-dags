4df7ff3e57d2a382629ca640238958f0c66105a7
eloynav <81321529+eloynav@users.noreply.github.com>
2025-03-13 16:16:54 -0700

    Add files via upload

--- dags/ice_unzip_load_daily - tasks.py
+++ dags/ice_unzip_load_daily - tasks.py
@@ -108,11 +108,11 @@ def daily_load_data():
                                     new_lst = [row[x] for x in range(19)]
                                     new_lst = new_lst + lst
                                     new_lst = new_lst + [row[x] for x in range(20,23)]
-                                    outfile.write(new_lst)
+                                    outfile.write({new_lst})
                                     #writer.writerow(new_lst)
                                 else:
                                     new_lst = [row[x] for x in range(21)]
-                                    outfile.write(new_lst)
+                                    outfile.write({new_lst})
                                     #writer.writerow(new_lst)
                                 
             except Exception as e:
