c3ac4ed9f84d4eca2ffd49ed37e6620dc7e77803
eloynav <81321529+eloynav@users.noreply.github.com>
2025-03-17 14:50:09 -0700

    Add files via upload

--- dags/ice_unzip_load_daily - tasks.py
+++ dags/ice_unzip_load_daily - tasks.py
@@ -107,25 +107,16 @@ def daily_load_data():
                             
                             lst = ['']
                             for row in csv_reader:
-                                
-                                new_lst = ''
-                                for column in row:
-                                    new_lst = new_lst + csv_reader[column]
-                                
-                                write.writerow(new_lst)
-                                    
-                                  
-                                #content = list(row[i] for i in included_cols)
-                                #write.writerow(content)
-                                #if 'sip:' in row[19]:
-                                #    new_lst = [row[x] for x in range(19)]
-                                #    new_lst = new_lst + lst
-                                #    new_lst = new_lst + [row[x] for x in range(20,23)]
-                                #    writer.writerow(new_lst)
-                                #else:
-                                #    new_lst = column
-                                    #new_lst = [row[x] for x in range(21)]
-                                #    writer.writerow(column)
+                                if 'sip:' in row[19]:
+                                    new_lst = [row[x] for x in range(0,19)]
+                                    new_lst = new_lst + lst
+                                    new_lst = new_lst + [row[x] for x in range(20,23)]
+                                    #writer.writerow(new_lst)
+                                else:
+                                    new_lst = column
+                                    new_lst = [row[x] for x in range(21)]
+                                    #writer.writerow(column)
+                                writer.writerow(new_lst)
                                 
             except Exception as e:
                 logging.error(f"Error data fixing table Stat_CDR: {e}")
