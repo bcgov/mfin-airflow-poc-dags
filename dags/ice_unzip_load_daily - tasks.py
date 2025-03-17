ee1a10081b9611fc94db8578f48f7fceea3468e5
eloynav <81321529+eloynav@users.noreply.github.com>
2025-03-17 15:21:05 -0700

    Add files via upload

--- dags/ice_unzip_load_daily - tasks.py
+++ dags/ice_unzip_load_daily - tasks.py
@@ -99,7 +99,7 @@ def daily_load_data():
                 # Initialize SambaHook with your credentials and connection details
                 included_cols = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21,22,23]
                 with SambaHook(samba_conn_id="fs1_rmo_ice") as fs_hook:
-                    with fs_hook.open_file(source_path + output_file, 'w',newline='') as outfile:
+                    with fs_hook.open_file(source_path + output_file, 'w',newline='',quoting=csv.QUOTE_ALL) as outfile:
                         writer = csv.writer(outfile)
                         
                         with fs_hook.open_file(source_path + file,'r') as f:
@@ -107,14 +107,15 @@ def daily_load_data():
                             
                             lst = ['']
                             for row in csv_reader:
-                                if 'sip:' in row[19]:
-                                    new_lst = [row[x] for x in range(19)]
-                                    new_lst = new_lst + lst
-                                    new_lst = new_lst + [row[x] for x in range(20,23)]
-                                    writer.writerows(new_lst)
-                                else:
-                                    new_lst = [row[x] for x in range(21)]
-                                    writer.writerows(new_lst)
+                                writer.writerow(row)
+                                #if 'sip:' in row[19]:
+                                #    new_lst = [row[x] for x in range(19)]
+                                #    new_lst = new_lst + lst
+                                #    new_lst = new_lst + [row[x] for x in range(20,23)]
+                                #    writer.writerow(new_lst)
+                                #else:
+                                #    new_lst = [row[x] for x in range(21)]
+                                #    writer.writerow(new_lst)
                                 
                                 
             except Exception as e:
