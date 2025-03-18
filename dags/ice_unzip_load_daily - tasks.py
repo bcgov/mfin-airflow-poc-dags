cc6813d491669fd3f7a511d01e22f3dd396875fd
eloynav <81321529+eloynav@users.noreply.github.com>
2025-03-18 13:34:37 -0700

    Add files via upload

--- dags/ice_unzip_load_daily - tasks.py
+++ dags/ice_unzip_load_daily - tasks.py
@@ -114,20 +114,22 @@ def daily_load_data():
                             with fs_hook.open_file(source_path + output_file, 'w') as outfile:
                                 
                                 writer = csv.writer(outfile)
-                                if 'sip:' in row['EventSequence']:
-                                    d = ','.join(str(e) for e in row)
+                                writer.writerows(row["PrimaryKey"])
+                                #if 'sip:' in row['EventSequence']:
+                                #    d = ','.join(str(e) for e in row)
                                     
-                                    writer.writerows(row["PrimaryKey"],row["EventTim"],row["DSStatus"],row["ContactID"],
-                                                    row["EventID"],row["SwitchID"],row["ContactType"],row["CurrentState"],
-                                                    row["LastState"],row["LastStateDuration"],row["QueueID"],row["IntData1"],
-                                                    row["InData2"],row["IntDate3"],row["IntData4"],row["StrData1"],row["StrData2"],
-                                                    row["StrData3"],row["StrData4"],row["ServiceId"],row["RolledUp"],row["ContactType"])
-                                else:
-                                    writer.writerows(row["PrimaryKey"],row["EventTime"],row["DSTStatus"],row["ContactID"],
-                                                    row["EventID"],row["SwitchID"],row["ContactType"],row["CurrentState"],
-                                                    row["LastState"],row["LastStateDuration"],row["QueueID"],row["IntData1"],
-                                                    row["InData2"],row["IntDate3"],row["IntData4"],row["StrData1"],row["StrData2"],
-                                                    row["StrData3"],row["StrData4"],row["EventSequence"],row["ServerId"],row["RolledUp"])
+                                    #writer.writerows(row["PrimaryKey"],row["EventTim"],row["DSStatus"],row["ContactID"],
+                                    #                row["EventID"],row["SwitchID"],row["ContactType"],row["CurrentState"],
+                                    #                row["LastState"],row["LastStateDuration"],row["QueueID"],row["IntData1"],
+                                    #                row["InData2"],row["IntDate3"],row["IntData4"],row["StrData1"],row["StrData2"],
+                                    #                row["StrData3"],row["StrData4"],row["ServiceId"],row["RolledUp"],row["ContactType"])
+                                #else:
+                                    
+                                #    writer.writerows(row["PrimaryKey"],row["EventTime"],row["DSTStatus"],row["ContactID"],
+                                #                    row["EventID"],row["SwitchID"],row["ContactType"],row["CurrentState"],
+                                #                    row["LastState"],row["LastStateDuration"],row["QueueID"],row["IntData1"],
+                                #                    row["InData2"],row["IntDate3"],row["IntData4"],row["StrData1"],row["StrData2"],
+                                #                    row["StrData3"],row["StrData4"],row["EventSequence"],row["ServerId"],row["RolledUp"])
                                     
                                      
                                      
