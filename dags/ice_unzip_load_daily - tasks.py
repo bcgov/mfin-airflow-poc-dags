19bbdb644d8c06d14faead9f8d1c5ecbce171ad1
eloynav <81321529+eloynav@users.noreply.github.com>
2025-03-18 16:56:12 -0700

    Add files via upload

--- dags/ice_unzip_load_daily - tasks.py
+++ dags/ice_unzip_load_daily - tasks.py
@@ -1,5 +1,6 @@
 import csv, sys, argparse
 import os
+import numpy as np
 import pandas as pd
 from airflow.decorators import dag, task
 from airflow.providers.samba.hooks.samba import SambaHook
@@ -104,10 +105,14 @@ def daily_load_data():
                                "StrData1","StrData2","StrData3","StrData4","EventSequence","ServerId","RolledUp","Extra"]
                     with fs_hook.open_file(source_path + file,'r') as f:
                         csv_reader = pd.read_csv(f, header = None, usecols=[i for i in range(22)], on_bad_lines = "skip")
-                        csv_reader[12] = csv_reader[12].astype(int)
-                        csv_reader[13] = csv_reader[13].astype(int)
-                        csv_reader[14] = csv_reader[14].astype(int)
-                        csv_reader[15] = csv_reader[15].astype(int)
+                        if np.isfinite(csv_reader[12]):
+                            csv_reader[12] = csv_reader[12].astype(int)
+                        if np.isfinite(csv_reader[13]):
+                            csv_reader[13] = csv_reader[13].astype(int)
+                        if np.isfinite(csv_reader[14]):
+                            csv_reader[14] = csv_reader[14].astype(int)
+                        if np.isfinite(csv_reader[15]):
+                            csv_reader[15] = csv_reader[15].astype(int)
                         
                         #csv_reader = csv.reader(f)
                             
