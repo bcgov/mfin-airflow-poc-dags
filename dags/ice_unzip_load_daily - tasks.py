1c0f7a10eab72280c54b492814d8664b5882f926
eloynav <81321529+eloynav@users.noreply.github.com>
2025-03-18 17:14:41 -0700

    Add files via upload

--- dags/ice_unzip_load_daily - tasks.py
+++ dags/ice_unzip_load_daily - tasks.py
@@ -105,6 +105,7 @@ def daily_load_data():
                                "StrData1","StrData2","StrData3","StrData4","EventSequence","ServerId","RolledUp","Extra"]
                     with fs_hook.open_file(source_path + file,'r') as f:
                         csv_reader = pd.read_csv(f, header = None, usecols=[i for i in range(22)], on_bad_lines = "skip")
+                        csv_reader = csv_reader.fillna(0)
                         if np.isfinite(csv_reader[12]):
                             csv_reader[12] = csv_reader[12].astype(int)
                         if np.isfinite(csv_reader[13]):
