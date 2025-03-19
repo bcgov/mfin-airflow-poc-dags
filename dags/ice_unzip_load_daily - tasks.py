6b9723e3e696d2cde9820f20c09ee5583dd7b4ba
eloynav <81321529+eloynav@users.noreply.github.com>
2025-03-19 10:07:48 -0700

    Add files via upload

--- dags/ice_unzip_load_daily - tasks.py
+++ dags/ice_unzip_load_daily - tasks.py
@@ -104,7 +104,7 @@ def daily_load_data():
                                "LastState","LastStateDuration","QueueID","IntData1","InData2","IntDate3","IntData4",
                                "StrData1","StrData2","StrData3","StrData4","EventSequence","ServerId","RolledUp","Extra"]
                     with fs_hook.open_file(source_path + file,'r') as f:
-                        csv_reader = pd.read_csv(f, header = None, usecols=[i for i in range(22)], on_bad_lines = "skip")
+                        csv_reader = pd.read_csv(f, header = None, usecols=[i for i in range(22)], quoting=1, on_bad_lines = 'skip')
                         #csv_reader = csv_reader.fillna(0)
                         #if len(csv_reader[12]) != 0:
                         #    csv_reader[12] = csv_reader[12].astype(int)
@@ -166,7 +166,7 @@ def daily_load_data():
             dYmd = (dt.datetime.today() + timedelta(days=-1)).strftime('%Y%m%d')
 
             try:
-                if psource_file == 'Stat_CDR.csv':
+                if psource_file == 'Stat_CDR_fixed.csv':
                     psource_file = 'Stat_CDR_fixed.csv'
                     pTableName = 'ICE_Stat_CDR'
                 elif psource_file == 'Stat_CDR_Summary.csv':
