b5bd4c96f5fe2983fc667e200d8dc383d07e72e7
eloynav <81321529+eloynav@users.noreply.github.com>
2025-03-25 12:53:44 -0700

    Add files via upload

--- dags/ice_rmo_load_ondemand.py
+++ dags/ice_rmo_load_ondemand.py
@@ -115,11 +115,11 @@ def ice_rmo_load_ondemand():
                         with fs_hook.open_file(source_path + output_file, 'w') as outfile:
                             csv_reader.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
                             
-                        df1 = csv_reader.loc[(csv_reader[22] ==  1) | (csv_reader[22] == 2)]    
+                        df1 = csv_reader.loc[(csv_reader[19].startswith("sip:")]    
                         df1 = df1.iloc[:,[0,1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21,22]]
                                         
-                        df2 = csv_reader.loc[(csv_reader[22] !=  1) | (csv_reader[22] !=  2)]    
-                        df2 = df2.iloc[:,[0,1,2,3,4,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]]
+                        df2 = csv_reader.loc[(csv_reader[19] >= 0) | (csv_reader[19] <= 100)]    
+                        df2 = df2.iloc[:,[0,1,2,3,4,8,9,10,11,12,13,14,15,16,17,18,19,20,21]]
                         
                         with fs_hook.open_file(source_path + output_file, 'w') as outfile:
                             df1.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
