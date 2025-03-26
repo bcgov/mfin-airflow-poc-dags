256e9f2dcca11eaa094408f4dabdf8d5af493152
eloynav <81321529+eloynav@users.noreply.github.com>
2025-03-25 18:29:06 -0700

    Add files via upload

--- dags/ice_rmo_load_ondemand.py
+++ dags/ice_rmo_load_ondemand.py
@@ -115,22 +115,10 @@ def ice_rmo_load_ondemand():
                         with fs_hook.open_file(source_path + output_file, 'w') as outfile:
                             csv_reader.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
                             
-                        df1 = csv_reader[csv_reader[19].str.len() > 50]
-                        df1 = df1.iloc[:, [0,1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21,22]]                      
+                        df1 = df1.iloc[:, [0,1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]]                      
                             
-                        #df1 = csv_reader.loc[len(csv_reader[19]) == 94]    
-                        #df1 = df1.iloc[:,[0,1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21,22]]
-                        df2 = csv_reader[csv_reader[19].str.len() < 3]                
-#                        df2 = csv_reader.loc[(len(csv_reader[19]) == 1) | (len(csv_reader[19]) == 2)]     
-                        df2 = df2.iloc[:,[0,1,2,3,4,8,9,10,11,12,13,14,15,16,17,18,19,20,21]]
-                        
                         with fs_hook.open_file(source_path + output_file, 'w') as outfile:
                             df1.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
-                                
-                        with fs_hook.open_file(source_path + output_file, 'a') as outfile:
-                            df2.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
-
-                                
   
                                 
                 outfile.close()
