dc149a0fa5ad3b48925f9d40c1c74c8b1921540b
eloynav <81321529+eloynav@users.noreply.github.com>
2025-03-26 17:18:33 -0700

    Add files via upload

--- dags/ice_rmo_load_ondemand.py
+++ dags/ice_rmo_load_ondemand.py
@@ -112,7 +112,7 @@ def ice_rmo_load_ondemand():
                     with fs_hook.open_file(source_path + file,'r') as f:
                         csv_reader = pd.read_csv(f, header = None, usecols=[i for i in range(22)], quoting=1)
  
-                    df1 = csv_reader.loc[:, [0,1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]]
+                    df1 = csv_reader.loc[:, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]]
  
                     with fs_hook.open_file(source_path + output_file, 'w') as outfile:
                             df1.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
@@ -154,10 +154,6 @@ def ice_rmo_load_ondemand():
                              "LastLoggedActionWfID","LastLoggedActionPageID","LastLoggedActionActionID","LastLoggedActionDuration",
                              "LastLoggedActionName","LastLoggedActionData","LastLoggedActionResult","ServerId"]
                                
-                    cols = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,
-                            20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,
-                            40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,
-                            60,61,62,63,64,65,66,67,68]                  
                     
                     with fs_hook.open_file(source_path + file,'r') as f:
                         csv_reader = pd.read_csv(f, header = None, usecols=[i for i in range(70)], quoting=1)
@@ -206,7 +202,7 @@ def ice_rmo_load_ondemand():
                     FROM '\\\\fs1.fin.gov.bc.ca\\rmo_ct_prod\\inprogress\\{psource_file}'
                     WITH
 	                ( FORMAT = 'CSV',
-                      MAXERRORS = 20, 
+                      MAXERRORS = 50, 
                       ERRORFILE='\\\\fs1.fin.gov.bc.ca\\rmo_ct_prod\\log\\{psource_file}.log'
 	                );
                 """
