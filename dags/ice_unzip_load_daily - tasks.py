e9ab6a64d0c3f5633e1d23d21651f15daa655d3b
eloynav <81321529+eloynav@users.noreply.github.com>
2025-04-30 17:01:50 -0700

    Add files via upload

--- dags/ice_unzip_load_daily - tasks.py
+++ dags/ice_unzip_load_daily - tasks.py
@@ -430,22 +430,22 @@ def daily_load_data():
  
         # Task 4: Removing 103 csv data files from \\fs1.fin.gov.bc.ca\rmo_ct_prod\inprogress\ subfolder
             
-        def remove_csv_inprogress():
-            conn_id = 'fs1_rmo_ice'
+    def remove_csv_inprogress():
+        conn_id = 'fs1_rmo_ice'
       
-            delete_path = r'/rmo_ct_prod/inprogress/'
-            hook = SambaHook(conn_id)
-            files = hook.listdir(delete_path)
+        delete_path = r'/rmo_ct_prod/inprogress/'
+        hook = SambaHook(conn_id)
+        files = hook.listdir(delete_path)
 
-            try:
-                for file in files:
-                    file_path = f"{delete_path}/{file}"
-                    hook.remove(file_path)
-            except:
-                logging.error(f"Error {e} removing file: {file_path}")
+        try:
+            for file in files:
+                file_path = f"{delete_path}/{file}"
+                hook.remove(file_path)
+        except:
+            logging.error(f"Error {e} removing file: {file_path}")
 
 
     #Set task dependencies
-    remove_csv_inprogress >> unzip_move_file() >> daily_load_source() #>> remove_csv_inprogress()
+    unzip_move_file() >> daily_load_source() #>> remove_csv_inprogress()
     
 dag = daily_load_data()
