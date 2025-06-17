5fad21379ca852aed1b674baf8625cfa2e1afde8
eloynav <81321529+eloynav@users.noreply.github.com>
2025-06-17 14:38:02 -0700

    Add files via upload

--- dags/email_notification.py
+++ dags/email_notification.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.python import BashOperator
 from airflow.providers.smtp.notifications.smtp import send_smtp_notification
 from datetime import datetime
 import logging
@@ -13,24 +14,31 @@ handler.setFormatter(formatter)
 root.addHandler(handler) 
 
 
-def send_email_smtp():
-    send_smtp_notification(
-        from_email="FINDAMSG@gov.bc.ca",
-        to="eloy.mendez@gov.bc.ca",
-        subject = "Missing daily source file iceDB_ICE_BCMOFRMO.zip",
-        html_content = "daily source file iceDB_ICE_BCMOFRMO.zip not available for loading"
-	)
-	
 with DAG(
     dag_id="email_missing_daily_load",
     schedule_interval = None,  # Set your schedule interval or leave as None for manual trigger
     start_date = datetime(2025,1,1),
     catchup = False,
-    tags = ["ice", "email", "missing","daily","load"]
-) as dag:
-    
-    email_task = PythonOperator(
-        task_id = 'send_email',
-        python_callable = send_email_smtp
+    tags = ["ice", "email", "missing","daily","load"],
+    on_failure_callback=[
+        send_smtp_notification(
+            from_email="FINDAMSG@gov.bc.ca",
+            to="eloy.mendez@gov.bc.ca",
+            subject="Missing daily source file",
+            html_content="daily source file iceDB_ICE_BCMOFRMO.zip not available for loading",
+        )
+    ],    
+):
+    BashOperator(
+        task_id="mytask",
+        on_failure_callback=[
+            send_smtp_notification(
+                from_email="FINDAMSG@gov.bc.ca",
+                to="eloy.mendez@gov.bc.ca",
+                subject="Missing daily source file",
+                html_content="daily source file iceDB_ICE_BCMOFRMO.zip not available for loading",
+            )
+        ],
+        bash_command="fail",
     )
 
