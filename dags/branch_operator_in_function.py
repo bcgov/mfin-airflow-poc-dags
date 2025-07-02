6c44c0ed72296ed6306bf039add981bc0f5be9ed
eloynav <81321529+eloynav@users.noreply.github.com>
2025-07-02 16:43:38 -0700

    Add files via upload

--- dags/branch_operator_in_function.py
+++ dags/branch_operator_in_function.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import BranchPythonOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
 from datetime import datetime
 from airflow.providers.samba.hooks.samba import SambaHook
