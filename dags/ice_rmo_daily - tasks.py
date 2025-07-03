314573e498901887dad5684b0dc76b65fa524055
eloynav <81321529+eloynav@users.noreply.github.com>
2025-07-03 15:43:30 -0700

    Add files via upload

--- dags/ice_rmo_daily - tasks.py
+++ dags/ice_rmo_daily - tasks.py
@@ -241,7 +241,7 @@ def etl_daily_load():
                 df3 = csv_reader.loc[(csv_reader[1] ==  1148)]  
                 df3 = df3.iloc[:,[0,1,2,3,4,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,34,35,36,37,38,39,40,41,43,44,45]]
 
-                 with fs_hook.open_file(pSourcePath + output_file, 'w') as outfile:
+                with fs_hook.open_file(pSourcePath + output_file, 'w') as outfile:
                     df1.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
                                 
                 with fs_hook.open_file(pSourcePath + output_file, 'a') as outfile:
