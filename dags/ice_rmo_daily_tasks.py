8ae551a77278c37ad8e61214e06bfa53a86c4181
eloynav <81321529+eloynav@users.noreply.github.com>
2025-07-07 12:03:50 -0700

    Add files via upload

--- dags/ice_rmo_daily_tasks.py
+++ dags/ice_rmo_daily_tasks.py
@@ -32,14 +32,6 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s -%(message
 handler.setFormatter(formatter)
 root.addHandler(handler) 
 
-#@dag(
-#    dag_id="ice_etl_load_daily_tasks",
-#    schedule_interval=None,  # Set your schedule interval or leave as None for manual trigger
-#    start_date=days_ago(1),
-#    catchup=False,
-#    tags=["ice", "etl", "unzip","load","inprogress_folder"]
-#)
-
 def email_notification():
         log_path = r'/rmo_ct_prod/log/'
         log_name = 'daily_backup.txt'
@@ -96,18 +88,17 @@ def etl_remove(pconn_id, plog_path, plog_name):
         with fs_hook.open_file(plog_path + plog_name,'a') as outfile:
             outfile.write("ETL step: 2, Task: Remove CSV Inprogress task, Time: %s\n" % dYmdHMS)
       
-    outfile.close()
-    DeletePath = Variable.get("vRMOSourcePath")
-    hook = SambaHook(conn_id)
-    files = hook.listdir(DeletePath)
+        outfile.close()
+        DeletePath = Variable.get("vRMOSourcePath")
+        files = fs_hook.listdir(DeletePath)
 
-    try:
-        for file in files:
-            file_path = f"{DeletePath}/{file}"
-            hook.remove(file_path)
+        try:
+            for file in files:
+                file_path = f"{DeletePath}/{file}"
+                fs_hook.remove(file_path)
         
-    except Exception as e:
-        logging.error(f"Error {e} removing file: {DeletePath}")
+        except Exception as e:
+            logging.error(f"Error {e} removing file: {DeletePath}")
             
     return
     
@@ -152,16 +143,15 @@ def etl_backup(pconn_id, plog_path, plog_name):
             SourcePath = '/rmo_ct_prod/'
             DestPath = '/rmo_ct_prod/completed/'
             file = 'iceDB_ICE_BCMOFRMO.zip'
-            hook = SambaHook(pconn_id)
             outfile.write("ETL step: 4, Task: Backup iceDB_ICE_BCMOFRMO-YYYYMMDD.zip task, Time: %s\n" % dYmdHMS)
-            #   Set dYmd to yesterdays date
+            # Set dYmd to yesterdays date
             dYmd = (dt.datetime.today() + timedelta(days=+2)).strftime('%Y%m%d')
             try:
-                files = hook.listdir(SourcePath)
+                files = fs_hook.listdir(SourcePath)
  
                 for f in files:
                     if f == 'iceDB_ICE_BCMOFRMO.zip':
-                        hook.replace(SourcePath + f, DestPath + 'iceDB_ICE_BCMOFRMO-' + dYmd+'.zip') 
+                        fs_hook.replace(SourcePath + f, DestPath + 'iceDB_ICE_BCMOFRMO-' + dYmd+'.zip') 
                     
             except Exception as e:
                 logging.error(f"Error backing up {file}-{dYmd}.zip source file")
@@ -181,26 +171,20 @@ def etl_truncate(pconn_id, plog_path, plog_name):
 
     outfile.close()
 
-    #sql_hook = MsSqlHook(mssql_conn_id='mssql_default')
     conn_id = 'mssql_default'
     conn = BaseHook.get_connection(pconn_id)
     dbname = 'FIN_SHARED_LANDING_DEV'
     host = conn.host
     user = conn.login
-    password = conn.password
-        
+    password = conn.password        
     connection = None
                 
     try:
         connection =  pymssql.connect(host = host, database = dbname, user = user, password = password)
-        cursor = connection.cursor()
-                    
+        cursor = connection.cursor()                    
         start_time = time.time()
-
-        cursor.execute("EXEC [dbo].[PROC_TELEPHONY_ICE_TRUNCATE]")
-            
-        connection.commit()            
-                                  
+        cursor.execute("EXEC [dbo].[PROC_TELEPHONY_ICE_TRUNCATE]")            
+        connection.commit()                                  
         logging.info(f"truncate landing tables {time.time() - start_time} seconds")
         
     except Exception as e:
@@ -220,173 +204,12 @@ def etl_daily_load():
     logging.basicConfig(level=logging.INFO)
         
        
-    def Agent_Datafix(pSourcePath):
-        file = 'Agent.csv'
-        output_file = 'Agent_fixed.csv'
-            
-        logging.info("Agent fixing code")
-            
-        try:
-            # Initialize SambaHook with your credentials and connection details
-            with SambaHook(samba_conn_id="fs1_rmo_ice") as fs_hook:
-                    
-                cols = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47]
-                    
-                with fs_hook.open_file(pSourcePath + file,'r') as f:
-                    csv_reader = pd.read_csv(f, header = None, usecols=cols, quoting=1)
-
-                df1 = csv_reader.loc[(csv_reader[1] ==  1137) | (csv_reader[1] == 1888) | (csv_reader[1] == 1889) | (csv_reader[1] == 1890) | (csv_reader[1] == 2001) | (csv_reader[1] == 2003) | (csv_reader[1] == 9985)]    
-                df1 = df1.iloc[:,[0,1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,32,33,34,35,36,37,38,39,41,42,43]]
-                                        
-                df2 = csv_reader.loc[(csv_reader[1] !=  1137) & (csv_reader[1] !=  1148) & (csv_reader[1] != 1888) & (csv_reader[1] != 1889) & (csv_reader[1] != 1890) & (csv_reader[1] != 2001) & (csv_reader[1] != 2003) & (csv_reader[1] != 9985)]    
-                df2 = df2.iloc[:,[0,1,2,3,4,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,36,37,38,39,40,41,42,43,45,46,47]]
-
-                df3 = csv_reader.loc[(csv_reader[1] ==  1148)]  
-                df3 = df3.iloc[:,[0,1,2,3,4,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,34,35,36,37,38,39,40,41,43,44,45]]
-
-                with fs_hook.open_file(pSourcePath + output_file, 'w') as outfile:
-                    df1.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
-                                
-                with fs_hook.open_file(pSourcePath + output_file, 'a') as outfile:
-                    df2.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
-                    
-                with fs_hook.open_file(pSourcePath + output_file, 'a') as outfile:
-                    df3.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
-                                
-                
-        except Exception as e:
-            logging.error(f"Error data fixing table Agent: {e}")
-                
-        return   
-
-
-    def Stat_CDR_Datafix(pSourcePath):
-        file = 'Stat_CDR.csv'
-        output_file = 'Stat_CDR_fixed.csv'
-
-        logging.info("Stat_CDR fixing code")
-        try:
-            # Initialize SambaHook with your credentials and connection details
-            with SambaHook(samba_conn_id="fs1_rmo_ice") as fs_hook:
-                    
-                cols = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]                  
-                    
-                with fs_hook.open_file(pSourcePath + file,'r') as f:
-                    csv_reader = pd.read_csv(f, header = None, usecols=[i for i in range(22)], quoting=1)
- 
-                df1 = csv_reader.loc[:, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]]
- 
-                with fs_hook.open_file(pSourcePath + output_file, 'w') as outfile:
-                    df1.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
-                                
-            outfile.close()
-                
-        except Exception as e:
-            logging.error(f"Error data fixing table Stat_CDR: {e}")
-               
-        return                     
-            
-    def Stat_CDR_Summary_Datafix(pSourcePath):
-        file = 'Stat_CDR_Summary.csv'
-        output_file = 'Stat_CDR_Summary_fixed.csv'
-
-        logging.info("Stat_CDR_Summary_fixing code")
-        try:
-            # Initialize SambaHook with your credentials and connection details
-            with SambaHook(samba_conn_id="fs1_rmo_ice") as fs_hook:
-                    
-                with fs_hook.open_file(pSourcePath + file,'r') as f:
-                    csv_reader = pd.read_csv(f, header = None, usecols=[i for i in range(70)], quoting=1)
- 
-                df1 = csv_reader.loc[:,[0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,
-                                        20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,
-                                        40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,
-                                        60,61,62,63,64,65,66,67,68,69]]                      
-                            
-
-                with fs_hook.open_file(pSourcePath + output_file, 'w') as outfile:
-                    df1.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
-  
-                                
-            outfile.close()
-                
-        except Exception as e:
-            logging.error(f"Error data fixing table Stat_CDR_Summary: {e}")
-                
-        return   
-        
-
-    def LOBCodeLangString(pSourcePath):
-        file = 'LOBCodeLangString.csv'
-        output_file = 'LOBCodeLangString_fixed.csv'
-
-        logging.info("LOBCodeLangString_fixing code")
-        try:
-            # Initialize SambaHook with your credentials and connection details
-            with SambaHook(samba_conn_id="fs1_rmo_ice") as fs_hook:
-                               
-                with fs_hook.open_file(pSourcePath + file,'r') as f:
-                    csv_reader = pd.read_csv(f, header = None, usecols=[i for i in range(3)], quoting=1)   
-
-                df1 = csv_reader.loc[:,[0,1,2]]
-                    
-                with fs_hook.open_file(pSourcePath + output_file, 'w') as outfile:
-                    df1.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
-                                
-            outfile.close()                    
-        
-        except Exception as e:
-            logging.error(f"Error data fixing table LOBCodeLangString {e}")
-                
-        return   
-
-            
-    def EvalCriteriaLangString(pSourcePath):
-        file = 'EvalCriteriaLangString.csv'
-        output_file = 'EvalCriteriaLangString_fixed.csv'
-
-        logging.info("EvalCriteriaLangString_fixing code")
-        try:
-            # Initialize SambaHook with your credentials and connection details
-            with SambaHook(samba_conn_id="fs1_rmo_ice") as fs_hook:
-                    
-                with fs_hook.open_file(pSourcePath + file,'r') as f:
-                    csv_reader = pd.read_csv(f, header = None, usecols=[i for i in range(3)], quoting=1)   
-
-                df1 = csv_reader.loc[:,[0,1,2]]
-                    
-                with fs_hook.open_file(pSourcePath + output_file, 'w') as outfile:
-                    df1.to_csv(outfile, header=False,index=False,lineterminator='\r\n')
-                                
-            outfile.close()                    
-        
-        except Exception as e:
-            logging.error(f"Error data fixing table LOBCodeLangString {e}")
-                
-        return       
-
-        
+       
     def load_db_source(pSourceFile, pDBName):
         sql_hook = MsSqlHook(mssql_conn_id='mssql_conn_bulk')
         dYmd = (dt.datetime.today() + timedelta(days = -1)).strftime('%Y%m%d')
 
         try:
-            #if pSourceFile == 'Stat_CDR.csv':
-            #    vTableName = 'ICE_Stat_CDR'
-            #    vSourceFile = 'Stat_CDR_fixed.csv'
-            #elif pSourceFile == 'Agent.csv':
-            #    vTableName = 'ICE_Agent'
-            #    vSourceFile = 'Agent_fixed.csv'
-            #elif pSourceFile == 'Stat_CDR_Summary.csv':
-            #    vTableName = 'ICE_Stat_CDR_Summary'
-            #    vSourceFile = 'Stat_CDR_Summary_fixed.csv' 
-            #elif pSourceFile == 'LOBCodeLangString.csv':   
-            #    vTableName = 'ICE_LOBCodeLangString'
-            #    vSourceFile = 'LOBCodeLangString_fixed.csv'
-            #elif pSourceFile == 'EvalCriteriaLangString.csv':   
-            #    vTableName = 'ICE_EvalCriteriaLangString'
-            #    vSourceFile = 'EvalCriteriaLangString_fixed.csv'     
-            #else:
             xlen = len(pSourceFile)-4
             vTableName = 'ICE_' + pSourceFile[:xlen]
             vSourceFile = pSourceFile
@@ -399,16 +222,6 @@ def etl_daily_load():
             vRMOInProgress = Variable.get("vRMOInProgressFolder")
             vRMOLog = Variable.get("vRMOLogFolder")
             
-#            query = f""" BULK INSERT [{pDBName}].[dbo].[{vTableName}]
-#                         FROM '{vRMOInProgress}{vSourceFile}'
-#                         WITH
-#	                     ( FORMAT='CSV', 
-#                           MAXERRORS=100, 
-#                           ERRORFILE='{vRMOLog}{vTableName}_{dYmd}.log',
-#                           TABLOCK 
-#	                     );
-#                     """
-
             query = f""" BULK INSERT [{pDBName}].[dbo].[{vTableName}]
                          FROM '{vRMOInProgress}{vSourceFile}'
                          WITH
@@ -444,13 +257,10 @@ def etl_daily_load():
                 outfile.write("ETL step: 6, Task: Loading csv data to FIN_SHARED_LANDING_DEV tables, Time: %s\n" % dYmdHMS)
 
     outfile.close()
-
         
     ConfigPath = Variable.get("vRMOConfigPath")
     FileName = Variable.get("vConfigName")
     SourcePath = Variable.get("vRMOSourcePath")
-    
-    
                 
     source_file_set=[] 
     data = []
@@ -470,13 +280,6 @@ def etl_daily_load():
     etl_backup(conn_id, log_path, log_name)
     etl_truncate(conn_id, log_path, log_name)
     
-    # Data fixes required for relevant daily table process 
-    #Agent_Datafix(SourcePath)
-    #Stat_CDR_Datafix(SourcePath)
-    #Stat_CDR_Summary_Datafix(SourcePath)
-    #LOBCodeLangString(SourcePath)
-    #EvalCriteriaLangString(SourcePath)
-              
     for source_file in data_set:
         load_db_source(source_file, DBName)
         
@@ -509,30 +312,6 @@ def create_dag():
         python_callable = choose_path,
         dag = dag  
     ) 
-    
-    #path_remove = PythonOperator(
-    #    task_id = 'path_remove',
-    #    python_callable = etl_remove,
-    #    dag = dag
-    #)
-    
-    #path_unzip = PythonOperator(
-    #    task_id = 'path_unzip',
-    #    python_callable = etl_unzip,
-    #    dag = dag
-    #)
-
-    #path_backup = PythonOperator(
-    #    task_id = 'path_backup',
-    #    python_callable = etl_backup,
-    #    dag = dag
-    #)
-
-    #path_truncate = PythonOperator(
-    #    task_id = 'path_truncate',
-    #    python_callable = etl_truncate,
-    #    dag = dag
-    #)
     
     path_daily_load = PythonOperator(
         task_id = 'path_daily_load',
