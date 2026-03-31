f71c16c384874e68b29211eed563009b0a79c9b7
eloynav <81321529+eloynav@users.noreply.github.com>
2026-03-30 20:58:35 -0700

    Add files via upload

--- dags/ice_rmo_load_ondemand_icepay.py
+++ dags/ice_rmo_load_ondemand_icepay.py
@@ -374,8 +374,8 @@ def ice_rmo_load_ondemand_icepay():
                     WITH
 	                 ( FIELDTERMINATOR = '|',
                        ROWTERMINATOR = '\r\n',
-                       MAXERRORS = 20, 
-                       ERRORFILE='\\\\fs1.fin.gov.bc.ca\\ptb_ct_prod\\log\\{pTableName}_{dYmd}.log',
+                       MAXERRORS = 20,
+                       ERRORFILE='\\\\fs1.fin.gov.bc.ca\\rmo_ct_prod\\log\\{pTableName}_{dYmd}.log',
                        TABLOCK 
 	                  );                  
                 """
