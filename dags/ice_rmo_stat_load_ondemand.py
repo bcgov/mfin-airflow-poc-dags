e13f782dcc9473e07c00870b620aff3ccb7dd111
eloynav <81321529+eloynav@users.noreply.github.com>
2026-06-23 09:13:15 -0700

    Add files via upload

--- dags/ice_rmo_stat_load_ondemand.py
+++ dags/ice_rmo_stat_load_ondemand.py
@@ -82,7 +82,7 @@ def ice_rmo_stat_load_ondemand():
     def ondemand_stat_load_data():
         
         source_file_set =  ["Transactions06.csv","Transactions07.csv","Transactions08.csv", "Transactions09.csv","Transactions10.csv"]
-                           #"Transactions11.csv","Transactions12.csv","Transactions13.csv", "Transactions14.csv","Transactions15.csv",
+                           #["Transactions11.csv","Transactions12.csv","Transactions13.csv", "Transactions14.csv","Transactions15.csv"]
                            #"Transactions16.csv","Transactions17.csv","Transactions18.csv", "Transactions19.csv","Transactions20.csv",
                            #"Transactions21.csv","Transactions22.csv","Transactions23.csv", "Transactions24.csv","Transactions25.csv",
                            #"Transactions26.csv","Transactions27.csv","Transactions28.csv", "Transactions29.csv","Transactions30.csv"]
